3dea762a00c0a4157a4236d753aebfa2d745e93b
hugov <nobukky19@gmail.com>
2024-01-26 22:04:53 +0100

    platforms

---
+++ Assets/Content/Scripts/Game/Platform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Platform : MonoBehaviour
+    {
+        [Header("Tweakable values")]
+        [SerializeField] private float _velocityThreshold;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.TryGetComponent<Player>(out var player)) return;
+            if (-player.rigidbody2D.velocity.y < _velocityThreshold) return;
+            
+            player.HandleDeath();
+        }
+    }
+}
---
+++ Assets/Content/Scripts/Game/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Player: MonoBehaviour
+    {
+        public Rigidbody2D rigidbody2D;
+        public GameObject graphicsParent;
+
+        public void HandleDeath()
+        {
+            Destroy(graphicsParent);
+        }
+    }
+}
