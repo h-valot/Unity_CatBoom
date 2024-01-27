3b10edf7d4232b6d946873af7c13b66332064188
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-01-27 20:27:48 +0100

    audio manager add

---
+++ Assets/Content/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip _musicMenu;
+    [SerializeField] private AudioClip _introSound;
+
+    [SerializeField] private RSE_Sound _rseSoundLaunch;
+    [SerializeField] private RSE_Sound _rseSoundStop;
+
+    private void Start() { _rseSoundLaunch.Call(TypeSound.Background, _musicMenu, true); }
+
+    public void PlaySoundPlay() { _rseSoundStop.Call(TypeSound.Background,_musicMenu,true); _rseSoundLaunch.Call(TypeSound.Background,_introSound,true); }
+}
