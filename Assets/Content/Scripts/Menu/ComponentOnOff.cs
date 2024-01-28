41f28534bb421ddc9f2c2d22aacd38b9f2210485
Verdraks <132646355+Verdraks@users.noreply.github.com>
2024-01-28 13:24:33 +0100

    add ui credits

---
+++ Assets/Content/Scripts/Menu/ComponentOnOff.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ComponentOnOff
+{
+    public bool isActif;
+    public GameObject obj;
+}
---
+++ Assets/Content/Scripts/Menu/OnOffComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffComponent : MonoBehaviour
+{
+    [SerializeField] List<ComponentOnOff> components;
+
+    public void OnClickOnOff()
+    {
+        for (int i = 0; i < components.Count; i++)
+        {
+            components[i].obj.SetActive(!components[i].isActif);
+            components[i].isActif = !components[i].isActif;
+        }
+    }
+}
